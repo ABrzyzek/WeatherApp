fd3bec416c8f10729922f11723b9471889f258c2
ABrzyzek <brzyzekadam@outlook.com>
2023-01-08 14:56:03 +0100

    chore: add updating function, fix get_weather_for_station

--- data/data_wunderground.py
+++ data/data_wunderground.py
@@ -10,9 +10,10 @@ from urllib.request import urlopen
 from data.schemas import Weather, Station
 
 
-def get_weather_for_station(station_code: str, start_date: datetime, end_date: datetime = date.today()) -> List[Weather]:
+def get_weather_for_station(station_code: str, start_date: datetime, end_date: datetime = date.today()) -> List[
+    Weather]:
     list_weather = []
-    while start_date.date() != end_date + timedelta(days=1):
+    while start_date != end_date + timedelta(days=1):
         date = start_date.strftime('%Y%m%d')
         url = f'https://api.weather.com/v1/location/{station_code}:9:PL/observations/historical.json?apiKey=e1f10a1e78da46f5b10a1e78da96f525&units=e&startDate={date}'
         response = urlopen(url)
@@ -26,7 +27,7 @@ def get_weather_for_station(station_code: str, start_date: datetime, end_date: d
             list_weather.append(weather)
             start_date += timedelta(minutes=30)
         if start_date.hour != 0 or start_date.minute != 0:
-            break
+            return list_weather
     return list_weather
 
 
@@ -36,13 +37,17 @@ def save_data_to_csv(weather_list: List[Weather]) -> None:
     data_set.to_csv(rf'files\{city}_{start_date}_{end_date}.csv', index=False, sep=';')
 
 
-def read_data_from_csv(filename: str, dataframe: bool = False) -> Union[List[Weather], pd.DataFrame]:
+def read_data_from_csv(filename: str) -> List[Weather]:
     data_set = pd.read_csv(rf'files/{filename}', sep=';')
     data_set = data_set.replace(np.nan, None)
     list_weather = []
     for row in data_set.T.to_dict().values():
         list_weather.append(Weather(**row))
-    return data_set if dataframe else list_weather
+    return list_weather
+
+
+def get_data_frame_from_csv(filename: str) -> pd.DataFrame:
+    return pd.DataFrame([model.dict() for model in read_data_from_csv(filename)])
 
 
 def get_station_names() -> List[Station]:
@@ -52,3 +57,22 @@ def get_station_names() -> List[Station]:
         for name, code in stations.items():
             list_stations.append(Station(name=name, code=code))
     return list_stations
+
+
+def get_code_for_city(city_name: str) -> str:
+    stations = get_station_names()
+    for station in stations:
+        if city_name == station.name:
+            return station.code
+    return ''
+
+
+def update_csv_for_new_data(filename: str):
+    list_weather = read_data_from_csv(filename)
+    last_date = list_weather[-1].date.date()
+    last_date = datetime(last_date.year, last_date.month, last_date.day)
+    station_code = get_code_for_city(list_weather[0].city)
+    new_list_weather = get_weather_for_station(start_date=last_date, station_code=station_code)
+    list_weather = [weather for weather in list_weather if weather not in new_list_weather]
+    list_weather += new_list_weather
+    save_data_to_csv(list_weather)
