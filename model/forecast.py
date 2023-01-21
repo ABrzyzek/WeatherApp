0a58686e428ec3377f9f278ecb32f872fd58b9cc
ABrzyzek <102187639+ABrzyzek@users.noreply.github.com>
2023-01-21 15:26:19 +0100

    Model (#4)
    * chore: add function getting df of variable and time and func creating dashboard for variable
    
    * chore: update requirements.txt
    
    * chore: add file forecast.py to predict functions and generate charts
    
    Signed-off-by: ABrzyzek <102187639+ABrzyzek@users.noreply.github.com>
    Co-authored-by: SESA682671 <adam.brzyzek@se.com>

--- model/forecast.py
+++ model/forecast.py
@@ -1,17 +1,27 @@
+from typing import Union
 import pandas as pd
 from matplotlib import pyplot as plt
 
 from data.data_wunderground import get_data_frame_from_csv
 
-def get_data_frame_for_variable(file_name: str, variable: str) -> pd.DataFrame:
+
+def get_data_frame_for_variable(file_name: str, variable: str) -> Union[pd.DataFrame, None]:
     data_set = get_data_frame_from_csv(file_name)
     column_names = data_set.columns.values.tolist()
-    return data_set[['date', variable]] if variable in column_names else None
+    if variable in column_names:
+        data_set = data_set[['date', variable]]
+        data_set = data_set.set_index('date')
+        return data_set
+    return None
 
 
 def get_dashboard_for_data(data: pd.DataFrame):
-    data = data.set_index('date')
     name = data.columns.values.tolist()[-1]
     data[[name]].plot(title=f'{name} dashboard',figsize = (12,8))
-    plt.show()
+    plt.savefig(f"data/files/{name}_chart.png", bbox_inches='tight')
 
+
+def get_prediction_holt_winters(data: pd.DataFrame):
+    name = data.columns.values.tolist()[-1]
+    print(data[name])
+    pass
