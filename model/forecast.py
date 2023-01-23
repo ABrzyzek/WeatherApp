288c0a0b6097ca2d275bba882de97171d0629000
ABrzyzek <102187639+ABrzyzek@users.noreply.github.com>
2023-01-24 00:43:09 +0100

    Update README and add func description (#6)
    * opisy forecast
    
    * Update README.md
    
    Signed-off-by: ABrzyzek <102187639+ABrzyzek@users.noreply.github.com>
    
    Signed-off-by: ABrzyzek <102187639+ABrzyzek@users.noreply.github.com>
    Co-authored-by: natkolasz <122165276+natkolasz@users.noreply.github.com>

--- model/forecast.py
+++ model/forecast.py
@@ -9,6 +9,9 @@ from data.schemas import Error, Prediction, PredictionRow
 
 
 def get_data_frame_for_variable(file_name: str, variable: str) -> Union[pd.DataFrame, None]:
+    '''!
+    Funkcja tworzy ramkę danych dla zmiennej oraz ustawia indeks jako datę
+    '''
     data_set = get_data_frame_from_csv(file_name)
     column_names = data_set.columns.values.tolist()
     if variable in column_names:
@@ -19,12 +22,20 @@ def get_data_frame_for_variable(file_name: str, variable: str) -> Union[pd.DataF
 
 
 def get_dashboard_for_data(data: pd.DataFrame, file_name: str):
+    '''!
+    Funkcja pozwalająca na uzyskanie nazwy kolumny (nazwy zmiennej) oraz generująca wykres uzyskanej zmiennej
+    '''
     name = data.columns.values.tolist()[-1]
     data[[name]].plot(title=f'{name} dashboard - {file_name}', figsize=(12, 8))
     plt.savefig(f"data/files/{name}_chart.png", bbox_inches='tight')
 
 
 def get_prediction_holt_winters(data: pd.DataFrame, file_name: str) -> Prediction:
+    '''!
+    Funkcja tworząca model predykcji Holta Wintera dla 6 kolejnych okresów (3 godz.).
+    Generuje wykres predykcji oraz wykres zależności testu i predykcji.
+    Liczy średni błąd kwadratowy oraz średni błąd bezwzględny.
+    '''
     name = data.columns.values.tolist()[-1]
     x = data[name]
     n = 6
