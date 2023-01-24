bd2102b35ad74ddeabad5775341736a56f83703a
kasia <kasia-podkowa@wp.pl>
2023-01-24 02:43:19 +0100

    unit tests for routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,5 @@
 from fastapi.testclient import TestClient
 from api.routes import app
-from datetime import datetime
 
 client = TestClient(app)
 
@@ -10,19 +9,18 @@ def test_stations_list():
     assert response.json()[0] == {"name": "Warsaw", "code": "EPWA"}
     assert response.json()[1] == {"name": "Gdansk", "code": "EPGD"}
 
-def test_station():
-    response = client.get("/weather/test")
-    date = datetime(2023,1,1)
-    date = date.isoformat()
+def test_weather():
+    response = client.get("/weather")
     assert response.status_code == 200
-    assert response.json()[0] == {"date":date, "city":"Warsaw", "temperature":57, "dev_point":48, "humidity":72, "wind":"SW", "wind_speed":10, "wind_gust":None, "pressure":29.57, "precipitation":None, "visibility":6, "heat_index":57, "weather_status":"Fair", "user_notes":None}
 
-def test_chart():
-    response = client.get("/chart/test")
+def test_csv_names():
+    response = client.get("/csvs")
+    test_file = "test.csv"
     assert response.status_code == 200
-    assert response.headers["content-type"] == "image/png"
+    assert test_file in response.json()
 
-def test_chart_invalid_variable():
-    response = client.get("/chart/invalid")
-    assert response.json() == {"error":"file_not_found"}
-    
+def test_dashboard_names():
+    response = client.get("/dashboards")
+    test_chart = "test_temperature_prediction_chart.png"
+    assert response.status_code == 200
+    assert test_chart in response.json()
